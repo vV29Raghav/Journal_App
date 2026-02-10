b5289191007c3551ab3bcf00394bd36fbf685288
vV29Raghav <vvrma923@gmail.com>
2026-02-11 01:55:51 +0530

    add role based auth - admin

--- src/main/java/net/raghav/journalApp/Controller/AdminController.java
+++ src/main/java/net/raghav/journalApp/Controller/AdminController.java
@@ -6,9 +6,7 @@ import net.raghav.journalApp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class AdminController {
             return new ResponseEntity<>(all, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createAdmin(@RequestBody User user) {
+        userService.saveAdmin(user);
     }
 }
--- src/main/java/net/raghav/journalApp/config/SpringSecurity.java
+++ src/main/java/net/raghav/journalApp/config/SpringSecurity.java
@@ -21,6 +21,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/journal/**", "/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
